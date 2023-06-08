d10f7b7223b6f4a47540b607d2535e1e2401eb0a
JennieGalmin <Jennie.Galmin@gmail.com>
2023-06-08 16:56:00 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+/* fetch('https://dummyjson.com/todos')
+.then(res => res.json())
+.then(console.log);*/
+
+const inputBox = document.getElementById("myInput");
+const list = document.getElementById("myUL");
+const addButton = document.getElementById("btn");
+
+function addTask{
+if {
+let li = document.createElement(li);
+
+}
+
+let newTask = document.createElement("SPAN");
+    let newText = document.createTextNode("");
+}
+
+
+    newTask.appendChild(newText);
+    document.body.appendChild(newTask);
+
+
+/*document.addEventListener("click", function(event){
+     if(!event.target.matches("#btn")) return;
+
+    fetch('https://dummyjson.com/todos')
+.then(res => res.json())
+.then((data)=> console.log(data));
+});
+*/
+
+    
+
+
+
+ 
+
+
+
+
+
+   
